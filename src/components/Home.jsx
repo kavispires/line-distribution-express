a0e3226914c43823d8090a334e0037b3412eed70
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-05-30 19:39:36 -0700

    Created Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from '../images/ld-express-logo.svg';
+import logo from '../images/ld-express-logo-negative.svg';
 
 function Home() {
   return (
     <main className="content home">
-      <p>Content comes here</p>
+      <img src={logo} className="home__logo" alt="logo" />
+      <p>Select a preset or create a new set to start distributing</p>
     </main>
   );
 }
